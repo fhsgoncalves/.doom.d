4e24ed89fcfba31b27aaeb0e78a742136687ab66
Fernando Gonçalves <fernando.hsgoncalves@gmail.com>
2021-02-27 15:02:55 -0300

    feat: add undo-tree; change js to use lsp; add aux functions; change theme and font; add bindings

--- config.el
+++ config.el
@@ -21,6 +21,8 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Monaco" :size 14 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Monaco" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -29,7 +31,8 @@
 ;;(setq doom-theme 'doom-one)
 ;;(setq doom-theme 'doom-material)
 ;;(setq doom-theme 'doom-nord)
-(set doom-theme 'doom-tomorrow-night)
+;;(set doom-theme 'doom-tomorrow-night)
+(set doom-theme 'doom-palenight)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -60,3 +63,8 @@
 ; examples of key bindings:
 ; (map! :leader "rb" #'+eval/buffer) 
 ; (map! "M-r" #'+eval/buffer)
+
+(map! "M-." #'lsp-find-definition)
+(map! "M-," #'lsp-find-references)
+
+(map! "M-;" #'comment-or-uncomment-region-or-line)
--- custom.el
+++ custom.el
@@ -44,3 +44,44 @@
 (setq sqlformat-args '("-s2" "-g"))
 (add-hook 'sql-mode-hook 'sqlformat-on-save-mode)
 
+;;  ---------------------------
+;;  typescript
+;;  ---------------------------
+(defun lsp-js-ts-rename-file ()
+  "Rename current file and all it's references in other files."
+  (interactive)
+  (let* ((name (buffer-name))
+         (old (buffer-file-name))
+         (basename (file-name-nondirectory old)))
+    (unless (and old (file-exists-p old))
+      (error "Buffer '%s' is not visiting a file." name))
+    (let ((new (read-file-name "New name: " (file-name-directory old) basename nil basename)))
+      (when (get-file-buffer new)
+        (error "A buffer named '%s' already exists." new))
+      (when (file-exists-p new)
+        (error "A file named '%s' already exists." new))
+      (lsp--send-execute-command
+       "_typescript.applyRenameFile"
+       (vector (list :sourceUri (lsp--buffer-uri)
+                     :targetUri (lsp--path-to-uri new))))
+      (mkdir (file-name-directory new) t)
+      (rename-file old new)
+      (rename-buffer new)
+      (set-visited-file-name new)
+      (set-buffer-modified-p nil)
+      (lsp-disconnect)
+      (setq-local lsp-buffer-uri nil)
+      (lsp)
+      (lsp--info "Renamed '%s' to '%s'." name (file-name-nondirectory new)))))
+
+;;  ---------------------------
+;;  comments
+;;  ---------------------------
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
